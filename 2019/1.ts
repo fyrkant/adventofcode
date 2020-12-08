43a86547a1e8b808a098ff9785a13f8781b1f484
Mattias Wikstrom <mattias.wikstrom@storytel.com>
2020-12-08 21:56:29 +0100

    preparation, create script

---
+++ 2019/1.ts
@@ -0,0 +1,2 @@
+import { data } from "./data/1.ts";
+import { assertEquals } from "https://deno.land/std@0.79.0/testing/asserts.ts";
---
+++ 2020/9.ts
@@ -0,0 +1,2 @@
+import { data } from "./data/9.ts";
+import { assertEquals } from "https://deno.land/std@0.79.0/testing/asserts.ts";
