12500834fe67b01f7c7166137c6ef851b4f1112b
Mattias Wikstrom <mattias.wikstrom@storytel.com>
2020-12-15 06:01:52 +0100

    13

--- 2020/13.ts
+++ 2020/13.ts
@@ -1,5 +1,5 @@
 import { data } from "./data/13.ts";
-// import { assertEquals } from "../assert.bundle.js";
+import { assertEquals } from "../assert.bundle.js";
 import { splitMap } from "../utils.ts";
 
 const parseLine = (input: string) => {
@@ -64,59 +64,73 @@ const getEarliestBus = (input: Data) => {
 };
 
 // assertEquals(getEarliestBus(makeData(testData)), 295);
-// assertEquals(getEarliestBus(makeData(data)), 222);
+assertEquals(getEarliestBus(makeData(data)), 222);
 
 const allMatch = (
   timestamp: number,
+  os: number,
   buses: ReturnType<typeof getBuses>,
 ): boolean => {
-  for (let index = 0; index < buses.length; index++) {
-    const [id, offset] = buses[index];
-    if ((timestamp + offset) % id !== 0) {
-      return false;
-    }
-  }
-  return true;
-  // return !buses.some(([id, offset]) => (timestamp + offset) % id !== 0);
+  // for (let index = 0; index < buses.length; index++) {
+  //   const [id, offset] = buses[index];
+  //   if ((timestamp + offset - os) % id !== 0) {
+  //     return false;
+  //   }
+  // }
+  // return true;
+  return buses.every(([id, offset]) => (timestamp + offset - os) % id === 0);
 };
 
 const findEarliestTimestamp = (
   start: number,
   buses: ReturnType<typeof getBuses>,
 ): number => {
-  let timestamp = start;
+  const initial = buses[0]; //.sort((a, b) => b[0] - a[0])[0];
+  let timestamp = initial[0];
+  let count = 0;
   while (true) {
-    // console.log(timestamp);
-    if (allMatch(timestamp, buses)) {
-      return timestamp;
+    // console.log({ biggest, start, timestamp });
+    count++;
+    if (
+      buses.every(([id, offset]) =>
+        (timestamp + offset - initial[1]) % id === 0
+      )
+    ) {
+      console.log({ count });
+      return timestamp - initial[1];
     } else {
-      timestamp = timestamp + buses[0][0];
+      timestamp = timestamp + initial[0];
+      // if (start % 2 !== 0 && timestamp % 2 === 0) {
+      //   timestamp = timestamp + biggest[0];
+      // }
     }
   }
 };
-
-const doFindEarliest = (input: string) => {
+const doFindEarliest = (input: string, start?: number) => {
   const d = getBuses(input);
-  const first = 99999999999972;
-  console.log({ first });
-  let x = 0;
-  for (let index = first; !allMatch(index, d); index = index + first) {
-    // console.log(index);
-    x = index;
-  }
-  const result = x + first;
-  console.log({ result });
-  return result;
-  // return findEarliestTimestamp(d[0][0], d);
+  const first = d[0][0];
+  // console.log({ first, start });
+  // // let x = 0;
+  // for (
+  //   let index = start || first;
+  //   !allMatch(index, d);
+  //   index = index + first
+  // ) {
+  //   // console.log(index);
+  //   xx = index;
+  // }
+  // const result = xx + first;
+  // console.log({ result });
+  // return result;
+  return findEarliestTimestamp(start || first, d);
 };
+assertEquals(doFindEarliest("17,x,13,19"), 3417);
+assertEquals(doFindEarliest("67,7,59,61"), 754018);
+assertEquals(doFindEarliest("67,x,7,59,61"), 779210);
+assertEquals(doFindEarliest("67,7,x,59,61"), 1261476);
+assertEquals(doFindEarliest("1789,37,47,1889"), 1202161486);
+assertEquals(doFindEarliest(data.split("\n")[1], 100000000000000), 1202161486);
 
-// assertEquals(doFindEarliest("17,x,13,19"), 3417);
-// assertEquals(doFindEarliest("67,7,59,61"), 754018);
-// assertEquals(doFindEarliest("67,x,7,59,61"), 779210);
-// assertEquals(doFindEarliest("67,7,x,59,61"), 1261476);
-// assertEquals(doFindEarliest("1789,37,47,1889"), 1202161486);
-// assertEquals(doFindEarliest(data), 1202161486);
-
-const x = doFindEarliest(data);
-console.log(x);
+// const x = doFindEarliest(data.split("\n")[1]);
+// console.log(x);
 // assertEquals(findEarliestTimestamp(29000000000, getBuses(data)), 123);
---
+++ 2020/14.ts
@@ -0,0 +1,151 @@
+import { data } from "./data/14.ts";
+import { assertEquals } from "../assert.bundle.js";
+
+const testData = `mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0`;
+
+const toBinary = (n: number) => {
+  return (n >>> 0).toString(2).padStart(36, "0");
+};
+
+const makeData = (input: string) => {
+  const arr = input.split("\n");
+  return arr.reduce((p, line, i, a) => {
+    if (line.startsWith("mask")) {
+      const [, mask] = line.split(" = ");
+      p.push({ mask, mems: [] });
+      return p;
+    }
+    const [mem, val] = line.split(" = ");
+    const [, pos] = /^mem\[([0-9]+)\]$/.exec(mem) || [];
+    p[p.length - 1].mems.push([parseInt(pos, 10), parseInt(val, 10)]);
+    return p;
+  }, [] as { mask: string; mems: [number, number][] }[]);
+};
+
+assertEquals(
+  makeData(testData),
+  [{
+    mask: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
+    mems: [[8, 11], [7, 101], [8, 0]],
+  }],
+);
+
+const maskBit = (value: number, mask: string) => {
+  const valueBits = toBinary(value).split(""); //.map((x) => parseInt(x, 10));
+
+  for (let index = 0; index < mask.length; index++) {
+    const element = mask[index];
+
+    if (element !== "X") {
+      // const v = parseInt(element, 10);
+      valueBits[index] = element;
+    }
+  }
+
+  const x = parseInt(valueBits.join(""), 2);
+  return x;
+};
+
+const fact = (num: number) => {
+  var rval = 1;
+  for (let i = 2; i <= num; i++) {
+    rval = rval * i;
+  }
+  return rval;
+};
+
+const getAllCombinations = (len: number) => {
+  const retArr: string[] = [];
+  const realLength = fact(len) + 2;
+
+  for (let index = 0; index < realLength; index++) {
+    retArr[index] = toBinary(index).slice(36 - len);
+  }
+
+  return retArr;
+};
+
+assertEquals(
+  getAllCombinations(3),
+  ["000", "001", "010", "011", "100", "101", "110", "111"],
+);
+
+const getFloatingBitPositions = (value: number, mask: string): number[] => {
+  const valueBits = toBinary(value).split(""); //.map((x) => parseInt(x, 10));
+  const returnArr: number[] = [];
+  let floatCount = 0;
+  for (let index = 0; index < mask.length; index++) {
+    const element = mask[index];
+
+    if (element === "1" || element === "X") {
+      // const v = parseInt(element, 10);
+      if (element === "X") {
+        floatCount++;
+      }
+      valueBits[index] = element;
+    }
+  }
+
+  const floatCombinations = getAllCombinations(floatCount);
+
+  const nums = floatCombinations.map((floatString) => {
+    let s = valueBits.join("");
+    for (let i = 0; i < floatString.length; i++) {
+      const element = floatString[i];
+
+      s = s.replace("X", element);
+    }
+    return parseInt(s, 2);
+  });
+  debugger;
+  return nums;
+};
+
+assertEquals(maskBit(11, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"), 73);
+
+const maskBits = (input: ReturnType<typeof makeData>) => {
+  const mem = new Map<number, number>();
+  for (let index = 0; index < input.length; index++) {
+    const { mask, mems } = input[index];
+    mems.forEach(([pos, val]) => {
+      mem.set(pos, maskBit(val, mask));
+    });
+  }
+  let sum = 0;
+  for (const v of mem.values()) {
+    sum = sum + v;
+  }
+
+  return sum;
+};
+const maskFloatingBits = (input: ReturnType<typeof makeData>) => {
+  const mem = new Map<number, number>();
+  for (let index = 0; index < input.length; index++) {
+    const { mask, mems } = input[index];
+    mems.forEach(([pos, val]) => {
+      getFloatingBitPositions(pos, mask).forEach((p) => {
+        // console.log({ p, val });
+        mem.set(p, val);
+      });
+    });
+  }
+  let sum = 0;
+  for (const v of mem.values()) {
+    sum = sum + v;
+  }
+  return sum;
+};
+
+// assertEquals(maskBits(makeData(testData)), 165);
+// assertEquals(maskBits(makeData(data)), 13476250121721);
+
+const testData2 = `mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1`;
+
+assertEquals(maskFloatingBits(makeData(testData2)), 208);
+console.log(maskFloatingBits(makeData(data)));
