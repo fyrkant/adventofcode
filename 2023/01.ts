ad192c2c6e0243d663087bc90ee66bf268234d41
Mattias Wikström <fyrkant@gmail.com>
2023-12-02 21:55:40 +0100

    2023 01/02

--- 2023/01.ts
+++ 2023/01.ts
@@ -1,3 +1,105 @@
-import { dataString } from './data/01.ts';
+import { dataString } from "./data/01.ts";
 import { assertEquals } from "https://deno.land/std@0.166.0/testing/asserts.ts";
 import * as mod from "https://deno.land/std@0.166.0/collections/mod.ts";
+
+const testInput = `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`;
+
+const parseLine = (input: string) => {
+  const nums = input.split("").filter((x) => {
+    return /[0-9]/.test(x);
+  });
+
+  return nums;
+};
+
+const vals = {
+  "one": "1",
+  "two": "2",
+  "three": "3",
+  "four": "4",
+  "five": "5",
+  "six": "6",
+  "seven": "7",
+  "eight": "8",
+  "nine": "9",
+};
+
+const findFirstorLast = (position: "first" | "last") => (input: string) => {
+  let i = position === "first" ? Infinity : -Infinity;
+  let v = "";
+
+  Object.entries(vals).forEach(([s, n]) => {
+    const si = position === "first" ? input.indexOf(s) : input.lastIndexOf(s);
+
+    if (si !== -1 && (position === "first" ? si < i : si > i)) {
+      i = si;
+      v = n;
+    }
+  });
+
+  Object.values(vals).forEach((n) => {
+    const ni = position === "first" ? input.indexOf(n) : input.lastIndexOf(n);
+
+    if (ni !== -1 && (position === "first" ? ni < i : ni > i)) {
+      i = ni;
+      v = n;
+    }
+  });
+
+  return v;
+};
+
+const findFirst = findFirstorLast("first");
+const findLast = findFirstorLast("last");
+
+Deno.test("findFirst", () => {
+  assertEquals(findFirst("eightwothree"), "8");
+  assertEquals(findFirst("4nineeightseven2"), "4");
+  assertEquals(findLast("eightwothree"), "3");
+  assertEquals(findLast("4nineeightseven2"), "2");
+});
+
+const parseLine2 = (input: string) => {
+  const first = findFirst(input);
+  const last = findLast(input);
+
+  return Number(`${first}${last}`);
+};
+
+const parseLines = (input: string) => {
+  return input.split("\n").map(parseLine).map((xs) => {
+    const first = xs.at(0);
+    const last = xs.at(-1);
+
+    return Number(`${first}${last}`);
+  });
+};
+
+const parseLines2 = (input: string) => {
+  return input.split("\n").map(parseLine2);
+};
+
+Deno.test("parseLines", () => {
+  assertEquals(parseLines(testInput), [12, 38, 15, 77]);
+});
+
+Deno.test("parseLines2", () => {
+  assertEquals(
+    parseLines2(`two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`),
+    [],
+  );
+});
+
+const part1 = mod.sumOf(parseLines(dataString), (x) => x);
+const part2 = mod.sumOf(parseLines2(dataString), (x) => x);
+
+console.log(part2);
--- 2023/02.ts
+++ 2023/02.ts
@@ -1,3 +1,99 @@
-import { dataString } from './data/02.ts';
+import { dataString } from "./data/02.ts";
 import { assertEquals } from "https://deno.land/std@0.166.0/testing/asserts.ts";
 import * as mod from "https://deno.land/std@0.166.0/collections/mod.ts";
+import { sumOf } from "https://deno.land/std@0.166.0/collections/sum_of.ts";
+
+const testData = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+
+const parseLine = (input: string) => {
+  const [game, setsString] = input.split(": ");
+  const [, gameNum] = game.split(" ");
+
+  const sets = setsString.split("; ").map((setString) =>
+    setString.split(", ").map((set) => {
+      const [n, c] = set.split(" ");
+
+      return { color: c as "red" | "green" | "blue", num: Number(n) };
+    })
+  );
+
+  return { gameNum: Number(gameNum), sets };
+};
+
+Deno.test("parseLine", () => {
+  assertEquals(
+    parseLine(
+      "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    ),
+    { gameNum: 3, sets: [] },
+  );
+});
+
+type MaxDefinition = { red: number; green: number; blue: number };
+
+const checkMaxDefinition = (
+  game: ReturnType<typeof parseLine>,
+  max: MaxDefinition,
+) => {
+  return game.sets.flat().every(({ color, num }) => {
+    return (color === "red" && num <= max.red) ||
+      (color === "green" && num <= max.green) ||
+      (color === "blue" && num <= max.blue);
+  });
+};
+
+Deno.test("checkMaxDefinition", () => {
+  assertEquals(
+    checkMaxDefinition(
+      parseLine(
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+      ),
+      { red: 12, green: 13, blue: 14 },
+    ),
+    false,
+  );
+});
+
+const partOne = (
+  input: string,
+  max: MaxDefinition,
+) => {
+  const parsedGames = input.split("\n").map(parseLine).filter((game) => {
+    return checkMaxDefinition(game, max);
+  });
+
+  const sum = mod.sumOf(parsedGames, (g) => g.gameNum);
+
+  console.log(sum);
+};
+
+const getBiggestColorNums = (game: ReturnType<typeof parseLine>) => {
+  const max = { red: 0, green: 0, blue: 0 };
+
+  game.sets.flat().forEach((set) => {
+    if (set.num > max[set.color]) {
+      max[set.color] = set.num;
+    }
+  });
+
+  return max;
+};
+
+const partTwo = (input: string) => {
+  const parsedGames = input.split("\n").map(parseLine).map((game) => {
+    const max = getBiggestColorNums(game);
+
+    return Object.values(max).reduce((p, c) => p * c, 1);
+  });
+
+  return sumOf(parsedGames, (x) => x);
+};
+
+// console.log(partOne(testData, { red: 12, green: 13, blue: 14 }));
+// console.log(partOne(dataString, { red: 12, green: 13, blue: 14 }));
+console.log(partTwo(testData));
+console.log(partTwo(dataString));
